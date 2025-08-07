b79b60a405e81e50c1ec8abdef4280921089a934
RobertPKyle <robertpetersonkyle2@gmail.com>
2025-08-07 16:12:44 -0500

    trying to fix stripe checkout 2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/book', bookingRoutes);
+
 app.use('/availability', availabilityRoutes);
 
 const PORT = process.env.PORT || 5000;
